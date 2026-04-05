5da488deef037c51d78aa1d48971ebdf01c06a90
FerMelero <fmeleromunoz@gmail.com>
2026-04-05 11:56:50 +0200

    Empezada función de añadir vehículos

--- models/db.py
+++ models/db.py
@@ -16,6 +16,34 @@ def crear_tablas():
 def get_all_vehicles():
     return session.query(Vehiculo).all()
 
+def insert_vehicle(marca, modelo, kilometros, vin, anio, motor, potencia, pCompra, pVenta, estado, fEntrada):
+    try:
+        nuevo_vehiculo = Vehiculo(
+            marca=marca,
+            modelo=modelo,
+            kilometros=kilometros,
+            vin=vin, 
+            anio=anio,
+            motor=motor,              
+            potencia=potencia,
+            precio_compra= pCompra,
+            precio_venta=pVenta,
+            estado="D",
+            fecha_entrada = fEntrada          
+        )
+
+        session.add(nuevo_vehiculo)
+        session.commit()
+    
+    except Exception as e:
+        session.rollback()
+        print("Error", e)
+    
+    finally:
+        session.close()
+
+
+
 
 if __name__ == "__main__":
     crear_tablas()
--- routes/vehicles.py
+++ routes/vehicles.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from models.db import get_all_vehicles # Importamos tu nueva función
+from flask import Blueprint, render_template, request
+from models.db import get_all_vehicles, insert_vehicle
 
 vehiculos_bp = Blueprint('vehiculos', __name__)
 
@@ -7,3 +7,8 @@ vehiculos_bp = Blueprint('vehiculos', __name__)
 def index():
     lista = get_all_vehicles()
     return render_template('index.html', vehiculos=lista)
+
+
+@vehiculos_bp.route("/nuevo", methods=["GET", "POST"])
+def new_vehicle():
+    if request.method == "POST":
